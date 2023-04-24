3a44cab91f8fed461c643061693babf723bd5bea
shale <shalevmatan421@gmail.com>
2023-04-24 15:01:59 -0400

    Layer class should be finalized

---
+++ src/neuralnetwork/Layer.java
@@ -0,0 +1,146 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package neuralnetwork;
+
+/**
+ *
+ * @author shale
+ */
+public class Layer {
+    
+    public double sigmoid(double input){
+        return (1/(1+Math.pow(Math.E,-input)));
+    }
+    
+    protected Layer previosLayer;
+    protected Layer nextLayer;
+    protected Perceptron[] perceptrons;
+    protected double[] perceptronValues;
+    protected Vector[] weights;
+    protected int size;
+    protected int numweights; //a variable intended to contain the number of expected weights between this and the last layer
+    
+    public Layer(int size){
+        this.size = size;
+        weights = new Vector[size];
+        perceptrons = new Perceptron[size];
+        nextLayer = null;
+        previosLayer = null;
+        for (int i = 0; i < size; i++) {
+            perceptrons[i] = new Perceptron(0,0);
+            perceptronValues[i] = 0;
+        }
+    }
+    
+    /**
+     * a method to link the current layer with a pre-existing layer
+     * @param previosLayer 
+     */
+    public static void link(Layer previosLayer, Layer nextLayer){
+        previosLayer.setNextLayer(nextLayer);
+        nextLayer.setPreviosLayer(previosLayer);
+        
+    }
+
+    public Layer getPreviosLayer() {
+        return previosLayer;
+    }
+
+    public void setPreviosLayer(Layer previosLayer) {
+        this.previosLayer = previosLayer;
+    }
+
+    public Layer getNextLayer() {
+        return nextLayer;
+    }
+
+    public void setNextLayer(Layer nextLayer) {
+        this.nextLayer = nextLayer;
+    }
+
+    public Perceptron[] getPerceptrons() {
+        return perceptrons;
+    }
+    
+    public Perceptron getPerceptron(int index) {
+        return perceptrons[index];
+    }
+
+    public void setPerceptrons(Perceptron[] perceptrons) {
+        this.perceptrons = perceptrons;
+        for (int i = 0; i < perceptrons.length; i++) {
+            perceptronValues[i] = perceptrons[i].getValue();
+        }
+    }
+    
+    public void setPerceptron(int index,Perceptron perceptron) {
+        perceptrons[index] = perceptron;
+        perceptronValues[index] = perceptron.getValue();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Vector[] getWeights() {
+        return weights;
+    }
+    
+    public Vector getWeight(int index) {
+        return weights[index];
+    }
+
+    public void setWeights(Vector[] weights) {
+        this.weights = weights;
+    }
+    
+    public void setWeight(int index, Vector weight) {
+        weights[index] = weight;
+    }
+    
+    /**
+     * a method to initialise the weights between the current and next layer
+     */
+    public void initialiseWeights() throws NextLayerDoesNotExistException{
+        
+        if (nextLayer == null){
+            throw new NextLayerDoesNotExistException();
+        }
+        
+        for (int i = 0; i < size; i++) {
+            weights[i] = new Vector(new double[nextLayer.getSize()]);
+        }
+    }
+    
+    /**
+     * a method to set the weights for one of the perceptrons in the next layer
+     * @param index the index of the perceptron in the next layer to set the weights for
+     * @param weights the new set of weights
+     */
+    public void setweights(int index, Vector weights){
+        this.weights[index] = weights;
+    }
+    
+    /**
+     * a method that uses the existing wights to calculate the values of the perceptrons in the next Layer
+     */
+    public void calcNextLayer() throws NextLayerDoesNotExistException{
+        double val;
+        Vector values = new Vector(perceptronValues);
+        
+        if (nextLayer == null){
+            throw new NextLayerDoesNotExistException();
+        }
+        
+        for (int i = 0; i < nextLayer.getSize(); i++) {
+            
+            val = nextLayer.getPerceptron(i).getBias()+ Vector.dotProduct(values, weights[i]);
+            
+            val = sigmoid(val);
+            
+            nextLayer.getPerceptron(i).setValue(val);
+        }
+    }
+}
---
+++ src/neuralnetwork/NextLayerDoesNotExistException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package neuralnetwork;
+
+/**
+ *
+ * @author shale
+ */
+public class NextLayerDoesNotExistException extends RuntimeException{}
