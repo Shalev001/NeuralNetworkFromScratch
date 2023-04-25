43145432b51c80975990f2e99560a9d327eacf06
shale <shalevmatan421@gmail.com>
2023-04-25 16:33:02 -0400

    implemented import method

--- src/neuralnetwork/Network.java
+++ src/neuralnetwork/Network.java
@@ -1,21 +1,24 @@
-
 package neuralnetwork;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 
 /**
  *
  * @author shale
  */
 public class Network {
-
+    
     Layer[] Layers;
-
+    
     public Network(int[] layerInfo) {
         Layers = new Layer[layerInfo.length];
         Layers[0] = new Layer(layerInfo[0]);
@@ -25,44 +28,45 @@ public class Network {
             Layers[i - 1].initialiseWeights();
         }
     }
-
-    public Layer getLayer(int index){
+    
+    public Layer getLayer(int index) {
         return Layers[index];
     }
     
-    public void compute(){
-        try{
-        for(Layer layer : Layers){
-            layer.calcNextLayer();
+    public void compute() {
+        try {
+            for (Layer layer : Layers) {
+                layer.calcNextLayer();
+            }
+        } catch (NextLayerDoesNotExistException e) {
         }
-        }catch(NextLayerDoesNotExistException e){}
     }
     
-    public void dumpOutput(){
-        Layers[Layers.length-1].dumpvals();
+    public void dumpOutput() {
+        Layers[Layers.length - 1].dumpvals();
     }
     
     public void generateRandomWeights() {
-
+        
         int size;
         double[] randweight;
         
         for (int i = 0; i < Layers.length - 1; i++) {
-
+            
             size = Layers[i].getSize();
-
+            
             for (int j = 0; j < Layers[i].getWeights().length; j++) {
                 
                 randweight = new double[size];
                 
-                for (int k = 0; k <  size; k++) {
+                for (int k = 0; k < size; k++) {
                     randweight[k] = Math.random();
                 }
                 Layers[i].setWeights(j, new Vector(randweight));
             }
         }
     }
-
+    
     public void generateRandomBiases() {
         for (int i = 1; i < Layers.length; i++) {
             for (Perceptron perceptron : Layers[i].getPerceptrons()) {
@@ -70,7 +74,7 @@ public class Network {
             }
         }
     }
-
+    
     public void export(File file) throws IOException {
         PrintWriter writer = new PrintWriter(
                 new BufferedWriter(
@@ -97,5 +101,70 @@ public class Network {
         }
         writer.flush();
         writer.close();
+    }
+    
+    public static Network importf(File file) throws FileNotFoundException, IOException {
+        
+        Network output = null;
+        
+        try {
+            BufferedReader reader = new BufferedReader(
+                new FileReader(file));
+            
+            String line = reader.readLine();
+            
+            int numLayers = Integer.parseInt(line);// getting the number of layers
+            int[] networkInfo = new int[numLayers];
+            
+            for (int i = 0; i < numLayers; i++) {
+                line = reader.readLine();
+                networkInfo[i] = Integer.parseInt(line);//getting the layer sizes and setting the array to match
+            }
+            
+            output = new Network(networkInfo);
+            
+            for (int i = 0; i < networkInfo.length; i++) {
+                Layer currentLayer = output.getLayer(i);
+                for (int j = 0; j < currentLayer.getPerceptrons().length; j++) {
+                    
+                    line = reader.readLine();
+                    
+                    double biasVal = Double.parseDouble(line);//getting all the biases
+                    
+                    currentLayer.getPerceptron(j).setBias(biasVal);
+                }
+            }
+            
+            for (int i = 0; i < networkInfo.length-1; i++) {
+                
+                Layer currentLayer = output.getLayer(i);
+                
+                for (int j = 0; j < currentLayer.getWeights().length; j++) {
+                    
+                    double[] temp = new double[networkInfo[i]];
+                    
+                    for (int k = 0; k < networkInfo[i]; k++) {
+                        
+                        line = reader.readLine();
+                        
+                        temp[k] = Double.parseDouble(line);
+                        
+                    }
+                    
+                }
+            }
+            
+            reader.close();
+            
+            return output;
+            
+        } catch (InputMismatchException e) {
+            System.out.println("file not in the correct formatt");
+            e.printStackTrace();
+            return output;
+           
+        }
+        
+        
     }
 }
